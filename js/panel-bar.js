040aa95606ccb4f2d0e9c462edf5694b8156d114
Cristian Sclifos <92810054+CSwebD@users.noreply.github.com>
2025-10-31 02:32:10 +0000

    Delete js directory

--- js/panel-bar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('../panel-bar.html')  // Adjusted path here
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('panel-bar-placeholder').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading panel bar:', error));
-});
